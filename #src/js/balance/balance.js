de5a151419fcf5c7d52b2370cf24a1f0d59b1ee9
kah3vich <70528062+kah3vich@users.noreply.github.com>
2021-11-09 22:22:11 +0300

    file

--- #src/js/balance/balance.js
+++ #src/js/balance/balance.js
@@ -9,4 +9,22 @@ var balanceSliderAbout = new Swiper(".balanceSliderAbout", {
     keyboard: true,
     slidesPerView: 4,
     spaceBetween: 0,
+    breakpoints: {
+        1410: {
+            slidesPerView: 4,
+            spaceBetweenSlides: 0
+        },
+        1110: {
+            slidesPerView: 3,
+            spaceBetweenSlides: 0
+        },
+        680: {
+            slidesPerView: 2,
+            spaceBetweenSlides: 0
+        },
+        0: {
+            slidesPerView: 1,
+            spaceBetweenSlides: 0
+        }
+    }
 });
