e0a22b7c65f5caea05780183eca35f5f400f99d5
kah3vich <70528062+kah3vich@users.noreply.github.com>
2021-11-16 21:12:37 +0300

    file

--- #src/js/constructor/constructor.js
+++ #src/js/constructor/constructor.js
@@ -4,3 +4,42 @@ $('.constructor-constructor__wrapper-controls-type').on('click', function() {
 $('.constructor-constructor__wrapper-controls-adds-close').on('click', function() {
     $('.constructor-constructor__wrapper-controls-adds').addClass('display-n')
 });
+
+
+
+let constructors = {
+    car: 1,
+    disk: 1,
+}
+
+$('#constructorTypeCarColor-1').on('click', function() {
+    const type = 1
+    constructors.car = type
+    constructorsChecked()
+});
+$('#constructorTypeCarColor-2').on('click', function() {
+    const type = 2
+    constructors.car = type
+    constructorsChecked()
+});
+
+$('#constructorTypeDiskColor-1').on('click', function() {
+    const type = 1
+    constructors.disk = type
+    constructorsChecked()
+});
+$('#constructorTypeDiskColor-2').on('click', function() {
+    const type = 2
+    constructors.disk = type
+    constructorsChecked()
+});
+
+function constructorsChecked() {
+    console.log(constructors)
+
+    if(constructors.disk == 1 && constructors.car == 2) {
+        $('#constructorCar').attr('src', './img/constructor/1-2.png')
+    }
+}
+
+
